f117e72a5b57b4233c20e002c42659c34a01a3c3
Krasimir Hristozov <krasi.hristozov@gmail.com>
2018-05-25 16:31:42 +0300

    Basic frontend which lists the movies in a table

---
+++ kills-tracker-client/src/components/Dashboard.vue
@@ -0,0 +1,34 @@
+<template>
+    <table>
+        <tbody>
+            <tr>
+                <th>ID</th>
+                <th>Title</th>
+                <th>Count</th>
+            </tr>
+            <template v-for="movie in movies">
+                <tr v-bind:key="movie.id">
+                    <td>{{ movie.id }}</td>
+                    <td>{{ movie.title }}</td>
+                    <td>{{ movie.count }}</td>
+                </tr>
+            </template>
+        </tbody>
+    </table>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    data() {
+        return {
+            movies: {}
+        }
+    },
+    async created () {
+        const response = await axios.get('http://localhost:8000/movies')
+        this.movies = response.data
+    }
+}
+</script>
--- kills-tracker-client/src/main.js
+++ kills-tracker-client/src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
-import App from './App.vue'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+import Dashboard from './components/Dashboard.vue';
+
+const routes = [
+  { path: '/', component: Dashboard },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
 
 new Vue({
-  render: h => h(App)
+  router,
+  render: h => h(Dashboard)
 }).$mount('#app')
